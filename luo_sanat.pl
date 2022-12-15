6698da8c3dc0338d1e87649859b607c298aa8997
  <>
2022-12-15 04:28:39 +0200

    sanat tehdään nollaerottimella

--- luo_sanat.pl
+++ luo_sanat.pl
@@ -2,18 +2,14 @@
 use utf8;
 use open ':std', ':encoding(UTF-8)';
 
-@lista = [];
-$pit = 0;
 %onko;
 open sisään, "<$ARGV[0]";
 foreach my $rivi (<sisään>) {
     $rivi = lc($rivi);
     while ($rivi =~ m/[a-z.,öä]{2,4}/g) {
 	if (!$onko{$&}++) {
-	    @lista[$pit++] = $&;
+	    print "$&\0";
 	}
     }
 }
 close sisään;
-
-print "@lista\n";
